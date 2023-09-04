cc4937b5e5c20a7b21406699700bc2d929e59a93
rohitdevs <133681597+rohitdevs@users.noreply.github.com>
2023-09-04 20:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve("Hello, world!"), ms));
+        }
+delay(1000).then(result => {
+                const resultDiv = document.getElementById('output');
+                resultDiv.innerText = result;
+            })
